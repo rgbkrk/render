2072d332ebe913356688b1049e1953ece10e88c3
Kyle Kelley <rgbkrk@gmail.com>
2024-02-26 13:17:10 -0800

    remove weird checkpoint file

--- src/spork/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Kyle Kelley <rgbkrk@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-
-from .models import ViewModel, AutoViewModel
-
-__all__ = ["ViewModel", "AutoViewModel"]
